c76db58b99312a9285881b54a3ec116cc66d8a48
Victor-Vansteenkiste <victor.vansteenkiste@hotmail.com>
2021-03-08 09:28:06 +0100

    test file voor pacman aangemaakt

---
+++ pacman/src/pacman/PacmanTest.java
@@ -0,0 +1,14 @@
+package pacman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PacmanTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
