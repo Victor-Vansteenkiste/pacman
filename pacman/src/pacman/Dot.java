0e1b7face53bbc8c64f8cb50a410f0271c88eafc
Victor-Vansteenkiste <victor.vansteenkiste@hotmail.com>
2021-03-08 08:59:54 +0100

    eerste versie van dot geschreven --> documentatie is nog niet volledig

--- pacman/src/pacman/Dot.java
+++ pacman/src/pacman/Dot.java
@@ -4,9 +4,26 @@ package pacman;
  * Each instance of this class represents a dot (a piece of food for Pac-Man) in a Pac-Man maze.
  */
 public class Dot {
+	/**
+	 * representationobject
+	 */
+	private Square square;
 	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
+	public Square getSquare() { return square; }
 	
-	public Dot(Square square) { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * Constructor for the class dot.
+	 * 
+	 * @throws IllegalArgumentException if the given square is null.
+	 * 		| square == null
+	 * 
+	 * @post the square of the instance equals the given square.
+	 * 		| getSquare() == square
+	 */
+	public Dot(Square square) { 
+		if(square == null)
+			throw new IllegalArgumentException("square is null");
+		this.square = square;
+	}
 
 }
