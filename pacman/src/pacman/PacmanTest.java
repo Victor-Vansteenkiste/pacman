2ba9d49c7c1b77a161ddf269e62a0c3d193804ca
Ferren Verhoeven <ferrenverhoeven@hotmail.com>
2021-03-08 09:42:17 +0100

    Test PacMan

--- pacman/src/pacman/PacmanTest.java
+++ pacman/src/pacman/PacmanTest.java
@@ -12,3 +12,69 @@ class PacmanTest {
 	}
 
 }
+package pacman;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PacManTest {
+	@Test
+	void test() {
+		Square square = new sqare()
+		PacMan pacman = new PacMan(3, square);
+	}
+}
+package pacman;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PacManTest {
+	@Test
+	void test() {
+		Square square = new sqare()
+		PacMan pacman = new PacMan(3, square);
+	}
+}
+package pacman;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PacManTest {
+	@Test
+	void test() {
+		Square square = new sqare()
+		PacMan pacman = new PacMan(3, square);
+	}
+}
+package pacman;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PacManTest {
+	@Test
+	void test() {
+		Square square1 = new Sqare();
+		square.rowIndex = 5;
+		square.columnIndex = 10;		
+		
+		PacMan pacman = new PacMan(1, square1);
+		assert pacman.getNbLives() == 1;
+		assert pacman.getSquare() == square1;
+		
+		Square square2 = new Square();
+		square.rowIndex = 7;
+		square.columnIndex = 2;
+		pacman.setSquare(square2);
+		assert pacman.getSquare() == square2;
+		
+		pacman.die();
+		assert pacman.getNbLives() == 0;
+		pacman.die();
+		assert pacman.getNbLives() == 0;
+		
+		
+	}
+}
