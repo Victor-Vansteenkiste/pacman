61cfe5a7479bd9f7b968f37520566002335480db
Victor-Vansteenkiste <victor.vansteenkiste@hotmail.com>
2021-03-11 14:54:09 +0100

    Adjustments to MazeMapTest

--- pacman/src/pacman/MazeMapTest.java
+++ pacman/src/pacman/MazeMapTest.java
@@ -8,16 +8,22 @@ class MazeMapTest {
  
 	@Test
 	void test() {
-		MazeMap map = new MazeMap(4, 4, new boolean[] {true, false, true, true, true, false, true, false, false, false, true, false, false, true, true, false});
-		assert map.getWidth() == 4;
-		assert map.getHeight() == 4;
-		assert map.isPassable(0, 0) == true;
-		assert map.isPassable(0, 1) == false;
+		// Creating the instance testMap of the class MazeMap.
+		MazeMap testMap = new MazeMap(4, 4, new boolean[] {true, false, true, true, true, false, true, false, false, false, true, false, false, true, true, false});
 		
-		MazeMap map2 = new MazeMap(1, 1, new boolean[] {true});
-		assert map2.getHeight() == 1;
-		assert map2.getWidth() == 1;
-		assert map.isPassable(0, 0) == true;
+		// Testing the basic methods
+		assert testMap.getWidth() == 4;
+		assert testMap.getHeight() == 4;
+		assert testMap.isPassable(0, 0) == true;
+		assert testMap.isPassable(0, 1) == false;
+		
+		// Creating the instance testMap2 of the class MazeMap
+		MazeMap testMap2 = new MazeMap(1, 1, new boolean[] {true});
+		
+		// Testing the basic methods
+		assert testMap2.getHeight() == 1;
+		assert testMap2.getWidth() == 1;
+		assert testMap.isPassable(0, 0) == true;
 	}
 
 }
